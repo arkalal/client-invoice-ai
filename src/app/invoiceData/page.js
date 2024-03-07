d3c7db4e45d59bfee45e56686422003b09946744
arkalal <arkalal.123@gmail.com>
2024-03-07 22:24:26 +0530

    testing export

--- src/app/invoiceData/page.js
+++ src/app/invoiceData/page.js
@@ -1,22 +1,22 @@
 import React from "react";
-import axios from "../../../axios/api";
-import InvoiceSheet from "../../../components/InvoiceSheet/InvoiceSheet";
+// import axios from "../../../axios/api";
+// import InvoiceSheet from "../../../components/InvoiceSheet/InvoiceSheet";
 
-const getInvoice = async () => {
-  try {
-    const res = await axios.get("invoices");
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const getInvoice = async () => {
+//   try {
+//     const res = await axios.get("invoices");
+//     return res.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 const InvoiceData = async () => {
-  const invoice = await getInvoice();
-  console.log("invoice", invoice);
+  // const invoice = await getInvoice();
+  // console.log("invoice", invoice);
   return (
     <div>
-      <InvoiceSheet invoice={invoice} />
+      {/* <InvoiceSheet invoice={invoice} /> */} <p>test</p>{" "}
     </div>
   );
 };
